4d532b0d0921d021166019bf469f25af83a60f63
UnluckyD <50873780+UnluckyD@users.noreply.github.com>
2019-05-21 21:11:50 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!';
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Запустился бот ${client.username}`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (msg.content === prefix + 'ping') {
         msg.reply('Pong!');
     }
 });
